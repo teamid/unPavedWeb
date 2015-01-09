c65275574acd8dbdab38d5e0f513348c981c144e
David Smith <teamid@gmail.com>
2015-01-08 17:33:56 -0800

    finished nav bar design

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+	var window_height = $(window).height();
+	var window_width = $(window).width();
+	$("#top-nav").height(window_height*0.0837)
+
+
+	$("#nav-content").width(window_width)
+
+
+
 
 })
