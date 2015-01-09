dad86dce31019ff2f0177a979b9f13e012d814d5
David Smith <teamid@gmail.com>
2015-01-08 21:36:16 -0800

    played with scroll over

--- js/app.js
+++ js/app.js
@@ -5,6 +5,22 @@ $(document).ready(function() {
 
 	$("#user-posts").height(window_height)
 
+	$("#helena-location, .gradient, #location-icon").hide()
+
+	$("#second-post").hover(function() {
+		$("#helena-location, .gradient, #location-icon").fadeIn(250);
+	}, function() {
+		$("#helena-location, .gradient, #location-icon").fadeOut(600);
+	});
+
+
+
+
+
+
+
+
+
 
 
 	$("#nav-content").width(window_width)
