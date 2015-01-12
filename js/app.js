f3c20b415437b9e2669f4d941c39df88fcbc4995
David Smith <teamid@gmail.com>
2015-01-12 09:21:16 -0800

    background height modifier

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 	var window_height = $(window).height();
 	var window_width = $(window).width();
 	$("#top-nav").height(window_height*0.0837);
-	
+
+	$("body").css( {
+		"background-size" : "100% window_height"
+
+	});
 
 	$("#user-posts").height(window_height)
 
