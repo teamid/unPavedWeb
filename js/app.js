76d84496aa4754ec0c1ebc7af02b5dd8b670bd4c
David Smith <teamid@gmail.com>
2015-01-12 09:15:52 -0800

    changing around

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var window_height = $(window).height();
 	var window_width = $(window).width();
 	$("#top-nav").height(window_height*0.0837);
+	
 
 	$("#user-posts").height(window_height)
 
